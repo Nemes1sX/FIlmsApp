9d67e727ddc57feadc5536707b14f7bd9a19dee9
Nemes1sX <kazstonk@gmail.com>
2022-12-27 16:20:54 +0200

    Added data context

---
+++ FIlmsApp/Data/FilmsContext.cs
@@ -0,0 +1,22 @@
+﻿using FIlmsApp.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FIlmsApp.Data
+{
+    public class FilmsContext : DbContext
+    {
+        public FilmsContext(DbContextOptions<FilmsContext> options) : base(options)
+        { 
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Film> Films { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
---
+++ FIlmsApp/Data/FilmsSeeding.cs
@@ -0,0 +1,43 @@
+﻿using FIlmsApp.Models.Entities;
+using FizzWare.NBuilder;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace FIlmsApp.Data
+{
+    public static class FilmsSeeding
+    {
+        public static void Seed(ModelBuilder builder)
+        {
+            builder.Entity<Genre>().HasData(
+                new Genre { Id = 1, Name = "Action"},
+                new Genre { Id = 2, Name = "Comedy" },
+                new Genre { Id = 3, Name = "Horror" },
+                new Genre { Id = 4, Name = "Thriller" },
+                new Genre { Id = 5, Name = "Drama" },
+                new Genre { Id = 6, Name = "Adventure" }
+                );
+
+            var random = new RandomGenerator();
+
+            var films = Builder<Film>.CreateListOfSize(100)
+                .All()
+                .With(m => m.Name = Faker.Name.FullName())
+                .With(m => m.GenreId = Faker.RandomNumber.Next(1,6))
+                .With(m => m.ReleasedDate = DateTime.Now.AddYears(-random.Next(0, 50)))
+                .Build();
+
+            builder.Entity<Film>().HasData(films);
+
+
+            var actors = Builder<Actor>.CreateListOfSize(100)
+             .All()
+             .With(a => a.Name = Faker.Name.FullName())
+             .With(a => a.Films = new List<Film> {films.ElementAt(Faker.RandomNumber.Next(1, 100)), films.ElementAt(Faker.RandomNumber.Next(1, 100)) })
+             .Build();
+
+
+            builder.Entity<Actor>().HasData(actors);
+        }
+    }
+}
