9bbfbdaf7ced5d4af497f04eda3515d1eb03cc9c
Nemes1sX <kazstonk@gmail.com>
2023-01-03 00:32:47 +0200

    Added validation rules

--- FIlmsApp/Models/FormRequest/BaseFormRequest.cs
+++ FIlmsApp/Models/FormRequest/BaseFormRequest.cs
@@ -1,7 +1,10 @@
-﻿namespace FIlmsApp.Models.FormRequest
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FIlmsApp.Models.FormRequest
 {
     public abstract class BaseFormRequest
     {
+        [Required, MinLength(3), MaxLength(255)]
         public string Name { get; set; }
     }
 }
--- FIlmsApp/Models/FormRequest/FilmFormRequest.cs
+++ FIlmsApp/Models/FormRequest/FilmFormRequest.cs
@@ -1,9 +1,12 @@
-﻿namespace FIlmsApp.Models.FormRequest
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FIlmsApp.Models.FormRequest
 {
     public class FilmFormRequest : BaseFormRequest
     {
         public int GenreId { get; set; }
         public int ActorId { get; set; }
+        [Required]
         public DateTime ReleasedDate { get; set; }
     }
 }
